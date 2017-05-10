142d655f104f9e8783d59b4ef0bd340233d35399
yuanxing <123>
2017-05-11 01:24:35 +0800

    cookie存放字符串作为值要先编码，取出时再解码 不然默认用asc码表。

--- src/com/mine/cookie/VisitServlet.java
+++ src/com/mine/cookie/VisitServlet.java
@@ -7,10 +7,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.swing.text.DateFormatter;
+import java.beans.Encoder;
 import java.io.IOException;
 import java.net.CookieManager;
 import java.net.HttpCookie;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Formatter;
 import java.util.List;
@@ -32,17 +36,20 @@ public class VisitServlet extends HttpServlet {
         response.setCharacterEncoding("UTF8");
         response.setContentType("text/html;charset=UTF-8");
         Cookie[] cookies = request.getCookies();
-        response.getWriter().println("<h1>hello! welcome to my site</h1>");
-        for (Cookie cookie : cookies) {
-            System.out.println(cookie.getName());
-            if (cookie.getName().equals(LAST_VISIT)) {
-                response.getWriter().print("<h2>your last visit time is " + cookie.getValue() + "</h2>");
-                break;
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                System.out.println(cookie.getName());
+                if (cookie.getName().equals(LAST_VISIT)) {
+                    response.getWriter().print("<h2>your last visit time is " + URLDecoder.decode(cookie.getValue(), "utf8") + "</h2>");
+                    break;
+                }
             }
+        } else {
+            response.getWriter().println("<h1>hello! welcome to my site</h1>");
         }
 //        每次访问都在cookie中存入这次访问的时间，作为下次访问时回显的时间。
-        String visitTime = DateFormat.getDateTimeInstance().format(new Date());
-        System.out.println(visitTime);
+        String time = SimpleDateFormat.getDateTimeInstance().format(new Date());
+        String visitTime = URLEncoder.encode(time, "UTF8");
         Cookie cookie = new Cookie(LAST_VISIT, visitTime);
         cookie.setMaxAge(3600);
         response.addCookie(cookie);
